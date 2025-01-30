94af45a64f643984dd5ebda2428f7a548578b48f
Gustavo Saraiva <124311748+saraivagustavo@users.noreply.github.com>
2025-01-30 16:57:37 -0300

    Add files via upload

--- Pacientes/pacientes.c
+++ Pacientes/pacientes.c
@@ -33,8 +33,7 @@ void f_printPaciente(Paciente *paciente) { // Função para imprimir os dados de
   printf("CPF: %s\n", paciente->cpf); // Imprime o CPF do paciente já formatado
   printf("Nome: %s\n", paciente->nome);   // Imprime o nome do paciente
   printf("Idade: %d\n", paciente->idade); // Imprime a idade do paciente
-  printf("Data de cadastro: %s\n",
-         paciente->data_cadastro); // Imprime a data de cadastro do paciente
+  printf("Data de cadastro: %s\n", paciente->data_cadastro); // Imprime a data de cadastro do paciente
 }
 
 void f_consultarPaciente(LinkedList *lista) {
@@ -63,10 +62,8 @@ void f_consultarPaciente(LinkedList *lista) {
       int encontrado = 0; // Flag para verificar se algum paciente foi encontrado
 
       while (atual) { // Percorre a lista
-        if (strstr(atual->paciente.nome,
-                   termoBusca)) { // Verifica se o nome contém o termo buscado
-          f_printPaciente(
-              &atual->paciente); // Imprime os dados do paciente encontrado
+        if (strstr(atual->paciente.nome, termoBusca)) { // Verifica se o nome contém o termo buscado
+          f_printPaciente(&atual->paciente); // Imprime os dados do paciente encontrado
           encontrado = 1;
         }
         atual = atual->next; // Avança para o próximo nó
@@ -88,8 +85,7 @@ void f_consultarPaciente(LinkedList *lista) {
       while (atual) { // Percorre a lista
         if (strcmp(atual->paciente.cpf, f_formataCPF(termoBusca)) ==
             0) { // Verifica se o CPF é igual ao buscado
-          f_printPaciente(
-              &atual->paciente); // Imprime os dados do paciente encontrado
+          f_printPaciente(&atual->paciente); // Imprime os dados do paciente encontrado
           encontrado = 1;
           break; // Encerra a busca após encontrar o paciente
         }
@@ -172,27 +168,20 @@ void f_atualizarPaciente(LinkedList *lista) {
                                                    // confirma a atualização
       scanf(" %c", &confirmacao);
 
-      if (confirmacao == 'S' ||
-          confirmacao == 's') { // Se a confirmação for 'S' ou 's', atualiza os
-                                // dados do paciente
-        if (cpfAtualizado[0] != '-') { // Se o novo CPF for diferente de '-',
-                                       // atualiza o CPF do paciente
+      if (confirmacao == 'S' || confirmacao == 's') { // Se a confirmação for 'S' ou 's', atualiza os dados do paciente
+        if (cpfAtualizado[0] != '-') { // Se o novo CPF for diferente de '-', atualiza o CPF do paciente
           strcpy(atual->paciente.cpf, cpfAtualizado);
         }
 
-        if (nomeAtualizado[0] != '-') { // Se o novo nome for diferente de '-',
-                                        // atualiza o nome do paciente
+        if (nomeAtualizado[0] != '-') { // Se o novo nome for diferente de '-',  atualiza o nome do paciente
           strcpy(atual->paciente.nome, nomeAtualizado);
         }
 
-        if (idadeAtualizada != -1) { // Se a nova idade for diferente de -1,
-                                     // atualiza a idade do paciente
+        if (idadeAtualizada != -1) { // Se a nova idade for diferente de -1, atualiza a idade do paciente
           atual->paciente.idade = idadeAtualizada;
         }
 
-        if (data_registroAtualizada[0] !=
-            '-') { // Se a nova data de cadastro for diferente de '-', atualiza
-                   // a data de cadastro do paciente
+        if (data_registroAtualizada[0] != '-') { // Se a nova data de cadastro for diferente de '-', atualiza a data de cadastro do paciente
           strcpy(atual->paciente.data_cadastro, data_registroAtualizada);
         }
 
@@ -223,8 +212,7 @@ void f_removerPaciente(LinkedList *lista) {
   ListNode *atual = lista->first; // Variável para percorrer a lista
 
   while (atual) { // Enquanto houver pacientes na lista
-    if (atual->paciente.id ==
-        id) { // Se o ID do paciente for igual ao ID digitado
+    if (atual->paciente.id == id) { // Se o ID do paciente for igual ao ID digitado
       // Exibir os dados do paciente antes de confirmar a remoção
       printf("\nPaciente encontrado:\n");
       printf("ID: %d\n", atual->paciente.id);
@@ -236,16 +224,12 @@ void f_removerPaciente(LinkedList *lista) {
       printf("\nDeseja realmente remover este paciente? (S/N): ");
       scanf(" %c", &confirmacao);
 
-      if (confirmacao == 'S' ||
-          confirmacao ==
-              's') { // Se a confirmação for 'S' ou 's', remove o paciente
+      if (confirmacao == 'S' || confirmacao == 's') { // Se a confirmação for 'S' ou 's', remove o paciente
         if (anterior) {
           // Se houver um nó anterior, o próximo nó do anterior será o próximo
-          // nó do nó atual
           anterior->next = atual->next;
         } else {
           // Se não houver um nó anterior, o primeiro nó da lista será o próximo
-          // nó do nó atual
           lista->first = atual->next;
         }
         free(atual);      // Libera a memória do nó atual
@@ -314,7 +298,7 @@ void f_inserirNovoPaciente(LinkedList *lista) {
   scanf(" %[^\n]", data_de_registro);
 
   // Confirmação do registro
-  printf("Confirma a inserção do registro acima? (S/N)\n");
+  printf("Confirma a inserção dos registros acima? (S/N)\n");
   scanf(" %c", &opcao);
 
   if (opcao == 'S' || opcao == 's') { 
@@ -324,15 +308,8 @@ void f_inserirNovoPaciente(LinkedList *lista) {
     strcpy(novoPaciente.nome, nome);
     novoPaciente.idade = idade;
     strcpy(novoPaciente.data_cadastro, data_de_registro);
+    f_adicionarNaLista(lista, &novoPaciente); // Adiciona o paciente à lista encadeada
 
-    printf("ID antes de adicionar: %d\n", novoPaciente.id);
-    printf("CPF antes de adicionar: %s\n", novoPaciente.cpf);
-    printf("Nome antes de adicionar: %s\n", novoPaciente.nome);
-    printf("Idade antes de adicionar: %d\n", novoPaciente.idade);
-    printf("Data de registro antes de adicionar: %s\n", novoPaciente.data_cadastro);
-
-    f_adicionarNaLista(lista,
-                       &novoPaciente); // Adiciona o paciente à lista encadeada
     printf("O registro foi concluído com sucesso.\n");
   } else {
     printf("Registro cancelado.\n");
